8ad052278e8b22cf3f9676adf4bd0be1a57f270a
Wei-Ting Yap <weitingyp@gmail.com>
2020-06-28 13:55:50 +0800

    Added rules and agree to rules (text reply)

--- bot.py
+++ bot.py
@@ -10,20 +10,16 @@ import logging
 load_dotenv('.env')
 BOT_TOKEN = environ.get('BOT_TOKEN')
 
-
 # Basic logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+YESNO = range(3)
+
 def info_to_str(user_data):
     """
-    Returns user data in string format:
-    user_data[property] - value
-    user_data[property] - value
-    ...
-    user_data[property] - value
-
+    Returns user data in string format.
 
     e.g. to use this function
     context.user_data['age'] = 20
@@ -32,40 +28,50 @@ def info_to_str(user_data):
     facts = list()
 
     for key, value in user_data.items():
-        facts.append(f'{key} - {value}')
+        facts.append(f'{key} - {value}') #user_data[property] - value
 
     return "\n".join(facts).join(['\n', '\n'])
 
 # Start callback function returns a greeting
 def start(update,context):
-    context.bot.send_message(chat_id=update.effective_chat.id,text="""
-        Welcome to Better To(gather)'s party-matching bot! We'll match you with other attendees with similar hobbies or interests. Exciting hor?
-    """)
+    user = update.effective_user
+    context.bot.send_message(chat_id=update.effective_chat.id,text=f"Hi @{user.username}! Welcome to Better To(gather)'s party-matching bot! We'll match you with other attendees with similar hobbies or interests. Exciting hor?")
 
-# Cue rules
-def rules(update,context):
-    """
-    We want to keep our Telegram page an open chat, but we are also a “family-friendly” page, so please keep comments and wall posts clean.
+    update.message.reply_text("""We want to keep our Telegram page an open chat, but we are also a “family-friendly” page, so please keep comments and wall posts clean.,
+        We want you to tell us what’s on your mind or provide a platform for likeminded individuals to connect through their interests, but if it falls into any of the categories below, we want to let you know beforehand that we will have to remove it:,
+        \n1. We do not allow graphic, obscene, explicit or racial comments or submissions nor do we allow comments that are abusive, hateful or intended to defame anyone or any organization.
+        \n2. We do not allow third-party solicitations or advertisements. This includes promotion or endorsement of any financial, commercial or non-governmental agency. Similarly, we do not allow attempts to defame or defraud any financial, commercial or non-governmental agency.
+        \n3. We do not allow comments that support or encourage illegal activity.
+        \nLet’s make this a safe space for everyone! :D
 
-    We want you to tell us what’s on your mind or provide a platform for likeminded individuals to connect through their interests, but if it falls into any of the categories below, we want to let you know beforehand that we will have to remove it:
+        Please send "Y" if you agree.""")
 
-    1. We do not allow graphic, obscene, explicit or racial comments or submissions nor do we allow comments that are abusive, hateful or intended to defame anyone or any organization.
+    return YESNO
 
-    2. We do not allow third-party solicitations or advertisements. This includes promotion or endorsement of any financial, commercial or non-governmental agency. Similarly, we do not allow attempts to defame or defraud any financial, commercial or non-governmental agency.
-
-    3. We do not allow comments that support or encourage illegal activity.
-
-    Let’s make this a safe space for everyone! :D
-    """
-    pass
+def yesno(update, context):
+    text = update.message.text
+    if text.lower() == "y":
+        update.message.reply_text("You have agreed to the rules")
+        return ConversationHandler.END
+    else:
+        update.message.reply_text("You must agree to the rules. Send \start to start again.")
+        return ConversationHandler.END
 
 def main():
     # Updater continuously fetches new updates from TG to be passed to Dispatcher
     updater = Updater(token=BOT_TOKEN,use_context=True)
     dispatcher = updater.dispatcher
 
-    start_handler = CommandHandler('start',start)
-    dispatcher.add_handler(start_handler)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start',start)],
+        states={
+            YESNO: [MessageHandler(Filters.text,
+                                    yesno)]
+        },
+        fallbacks=[]
+    )
+
+    dispatcher.add_handler(conv_handler)
 
     # Start bot
     updater.start_polling()
