72d1ab69b67387ebd5d405c4e270658d6b17541e
Wei-Ting Yap <weitingyp@gmail.com>
2020-07-25 16:41:30 +0800

    created coffeeDB class which can be imported into main bot.py to interface with sqlite3 database

--- db.py
+++ db.py
@@ -1,9 +1,25 @@
 import sqlite3
 
-conn = sqlite3.connect('coffeebot.db')
-c = conn.cursor()
+class coffeeDB():
+    """
+    Database helper function for coffee chatbot
+    coffeeDB.conn is the Connection object that represents the database
+    coffeeDB.c is the Cursor object to perform SQL commands
+    
+    More info about DB-API interface for SQLite databases:
+    https://docs.python.org/3/library/sqlite3.html#cursor-objects
+    """
 
-c.execute(''' CREATE TABLE users
+    def __init__ (self, dbname = 'scoffeebot.db'):
+        self.conn = sqlite3.connect(dbname)
+        self.c = self.conn.cursor()
+    
+    def setup(self):
+        '''declares database schema'''
+
+        self.c.execute(''' 
+                CREATE TABLE IF NOT EXISTS
+                users
                 (user_id INT PRIMARY KEY,
                 chat_id INT UNIQUE,
                 username VARCHAR(50) NOT NULL,
@@ -14,5 +30,10 @@ c.execute(''' CREATE TABLE users
                 matched INT DEFAULT 0
                 )
             ''')
+        self.conn.commit()
 
-conn.commit()
+if __name__ == "__main__":
+    db = coffeeDB()
+    db.setup()
+    db.c.execute(''' SELECT * FROM users ''')
+    print(db.c.fetchall())
